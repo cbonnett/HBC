52b515ff797b8f6df9ed5dc004336f94f7db93ba
muchtajidun <mateosmartinfelipe@gmail.com>
2015-11-13 10:51:34 +0100

    First Commit

---
+++ src/bhc.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  6 15:56:41 2015
+
+@author: felipemateosmartin
+"""
+
+from tree import tree
+import numpy as np
+<<<<<<< HEAD
+=======
+from tree import tree
+>>>>>>> felipe
+
+arbol = tree(np.arange(10),"")
+arbol.initTree()
+arbol.fit()
+
+        
+ 
---
+++ src/tree.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+<<<<<<< HEAD
+Created on Sat Nov  7 16:38:37 2015
+
+@author: felipemateosmartin
+"""
+
+=======
+Created on Sat Nov  7 20:56:22 2015
+
+@author: felipemateosmartin
+
+
+Class tree grows the Trre from botton to top merging nodes
+
+"""
+
+
+>>>>>>> felipe
+import numpy as np
+from node import node
+from itertools import permutations
+
+class tree():
+    
+    
+    def __init__(self,rawData,diffFunction):
+        self.rawData = rawData
+        self.diffFunction = diffFunction
+        self.iterNodes={}
+        self.nodes=[]
+        self.activeNodes=[]
+        self.node1=[node() for i,d in enumerate(rawData)]
+        print self.node1
+        
+    def initTree(self):
+        [self.node1[i].populateNode(i,[],self.diffFunction)  for i,d  in enumerate(self.rawData)]
+        [self.node1[i].initData(d)  for i,d  in enumerate(self.rawData)]
+        self.activeNodes = [node.id for node in self.node1]
+        print self.activeNodes
+        
+    def createPerm(self,nodesToMerge):
+        return permutations(nodesToMerge,2)
+    
+<<<<<<< HEAD
+    def mergedNodes(self):
+        nodes = list(self.createPerm(self.activeNodes))
+=======
+    def mergedNodes(self,node):
+        nodes = list(self.createPerm(self.activeNodes))
+        print "- 1 - Number of Nodes that could be merge : {0} ".format(len(self.activeNodes))
+>>>>>>> felipe
+        rkList=[]
+        for duple in nodes:
+            rk = np.random.rand()
+            rkList.append(rk)
+         
+<<<<<<< HEAD
+        #print rkList
+        mergeCandidate=nodes[rkList.index(np.max(rkList))]
+        print mergeCandidate
+        nodeMerge = self.node1()
+        nodeMerge.populate(max([node.id for node in nodes])+1,list(mergeCandidate),self.diffFunction)
+        
+        [self.activeNodes.remove(elem) for elem in nodeMerge.children]
+        self.activeNodes.append(nodeMerge.id)
+        
+        self.node1.append(nodeMerge.merge())
+        
+
+    def fit(self):
+        while ( len(self.activeNodes)!=1):
+                self.mergedNodes()
+    
+if __name__ == "__main__":
+
+=======
+
+        cadidateIndex = list(nodes[rkList.index(np.max(rkList))]) 
+        print "- 2 - Nodes to merge {0}".format(cadidateIndex)
+        mergeCandidate=[self.node1[cadidateIndex[0]],self.node1[cadidateIndex[1]]]
+        
+        nodeMerge = node()
+        nodeMergeId=max([i.id for i in self.node1])+1
+        print "- 3 - New Node Crated with id {0}".format(nodeMergeId)
+        nodeMerge.populateNode(nodeMergeId,mergeCandidate,self.diffFunction)
+        
+        
+        [self.activeNodes.remove(elem.id) for elem in nodeMerge.children]
+        self.activeNodes.append(nodeMerge.id)        
+        self.node1.append(nodeMerge.merge())
+        print "Node Created :"
+        self.node1[-1].toString()
+        
+
+    def fit(self):
+        count=0
+        while ( len(self.activeNodes)!=1):
+                count +=1
+                print "Loop number {0} : ___________________________________".format(count)                
+                self.mergedNodes(node)
+                
+    
+
+if __name__ == "__main__":
+>>>>>>> felipe
+    arbol = tree(np.arange(10),"")
+    arbol.initTree()
+    arbol.fit()
+
+        
