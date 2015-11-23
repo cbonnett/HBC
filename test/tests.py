c2cd888c025693fb12571f350599ea536f6fd4fb
muchtajidun <mateosmartinfelipe@gmail.com>
2015-11-23 18:38:08 +0100

    Add Class where to implemet and run all test

---
+++ test/tests.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 18:34:57 2015
+
+@author: felipemateosmartin
+"""
+
+class RunTest():
+    def __init__(self,strPrint):
+        self.strPrint = strPrint
+    def pr(self):
+        print self.strPrint
